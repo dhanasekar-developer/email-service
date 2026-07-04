a597947daa2cca1ceea1d5710fc6d34819cb3ec7
DHANASEKAR <71744@TCS.Local>
2026-07-04 13:54:09 +0530

    email router alter

--- app/email/router.py
+++ app/email/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 from .service import EmailService
 from app.db.database import db_dependency
 from .schema import SendEmailRequest
@@ -12,7 +13,19 @@ mail_service = EmailService()
 
 @router.post('/send_email', status_code=status.HTTP_200_OK)
 def send_email(request: SendEmailRequest, db: db_dependency):
-    print('request:0---------------',request)
-    mail_service.insert_email_record(request, db)
-    return 1
+    
+    email_record = mail_service.insert_email_record(request, db)
+
+    if email_record:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK
+        )
+
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                'detail': 'Something went wrong, Please try again!'
+            }
+        )
     
